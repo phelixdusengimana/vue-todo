7285a1aa328b6eaf2cc183c11209965778755710
phelixdusengimana <phelixdusengimana@gmail.com>
2021-01-19 15:23:52 +0200

    add

---
+++ src/components/Todo/AddTodo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="add__todo">
+       <form @submit="addTodo">
+           <input type="text" name="title" v-model="title"/>
+           <button type="submit">Add &#x2b;</button>
+       </form>
+    </div>
+</template>
+
+<script>
+import uuid from "uuid";
+export default {
+    name: "AddTodo",
+    data() {
+        return{
+            title: ''
+        }
+    },
+    methods:{
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
